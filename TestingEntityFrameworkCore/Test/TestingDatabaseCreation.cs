6702df4b3abc18ebff5fa0d7664c15a7ee6567ad
Vlad Vizitiu <vnvizitiu@gmail.com>
2018-07-06 17:04:48 +0300

    Implemented a test to showcase the logging;

---
+++ TestingEntityFrameworkCore/Test/TestingDatabaseCreation.cs
@@ -0,0 +1,18 @@
+﻿namespace Test
+{
+    using NUnit.Framework;
+
+    using Test.TestUtilities;
+
+    using WebApplication.Data;
+
+    [TestFixture]
+    public class TestingDatabaseCreation
+    {
+        [Test]
+        public void TestCreation()
+        {
+            ApplicationDbContext context = TestDatabaseContextFactory.CreateDbContext();
+        }
+    }
+}
